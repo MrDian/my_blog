46910312b32f0d78aeacec6d683709924cee9e9c
devon <wendian1989@126.com>
2017-06-14 00:11:50 +0800

    c

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,13 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'gagag' });
 });
 
+router.get('/api/get_safari_account', function(req, res, next) {
+
+	console.log(req.url);
+	res.json({
+		status: 0
+	});
+});
+
+
 module.exports = router;
